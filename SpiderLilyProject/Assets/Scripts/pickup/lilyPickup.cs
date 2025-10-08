1fb52785c09134baecc2c4109a748a0cc4ca2238
Marcos Hernandez <hmarcos480@gmail.com>
2025-10-08 14:02:55 -0400

    lily wip

--- SpiderLilyProject/Assets/Scripts/pickup/lilyPickup.cs
+++ SpiderLilyProject/Assets/Scripts/pickup/lilyPickup.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Lily")]
+public class lily : ScriptableObject
+{
+    [SerializeField] GameObject itemModel;
+   
+}
 public class lilyPickup : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
