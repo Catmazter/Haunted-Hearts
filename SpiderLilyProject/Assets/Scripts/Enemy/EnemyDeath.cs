c10e4e67e87dbc4c963b7d074172c4af97f8c9a6
Valeria <valemalegre.me@gmail.com>
2025-10-09 22:32:39 -0400

    enemy creating logic in progress

--- SpiderLilyProject/Assets/Scripts/Enemy/EnemyDeath.cs
+++ SpiderLilyProject/Assets/Scripts/Enemy/EnemyDeath.cs
@@ -1,16 +1,53 @@
+using System.Threading;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class EnemyDeath : MonoBehaviour
+public class EnemyDeath : EnemyBase
 {
+    [SerializeField] Animator anim;
+
+    [SerializeField] float playerNavmeshRadius = 1.0f;
+    [SerializeField] float selfDestruct = 5f;
+
+    float timerChase = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
         
+
+        startChasing();
+
+    }
+
+    protected override void chasePlayer()
+    {
+        agent.speed = chaseSpeed;
+        base.chasePlayer();
+
+    }
+
+    //bool PlayerOnNavMesh()
+    //{
+    //    return NavMesh.SamplePosition(gameManager.instance.player.transform.position, out _, playerNavmeshRadius, NavMesh.AllAreas); 
+    //    //checks around player if theres navmesh
+            //didn't worked: too many spots without navmesh (not reliable)
+    //}
+
+    private void startChasing()
+    {
+
+        if (timerChase > 0)
+        {
+            timerChase -= Time.deltaTime;
+            return;
+        }
+
+        chasePlayer();
     }
 }
