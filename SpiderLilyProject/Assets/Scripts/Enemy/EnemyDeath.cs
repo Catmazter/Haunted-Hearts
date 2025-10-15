d2808a9c1b64f2d35242ab68fbd12874faba5b06
Valeria <valemalegre.me@gmail.com>
2025-10-15 15:24:28 -0400

    Death roam in safezone complete

--- SpiderLilyProject/Assets/Scripts/Enemy/EnemyDeath.cs
+++ SpiderLilyProject/Assets/Scripts/Enemy/EnemyDeath.cs
@@ -5,13 +5,12 @@ using System.Collections;
 
 public class EnemyDeath : EnemyBase
 {
-    //[SerializeField] Animator anim;
 
     // ====timer==== //
     [SerializeField] float selfDestruct = 5f; //time player has to be inside safezone before enemy destroys
+    [SerializeField] float escapeDistance = 10f;
 
     float timerChase = 3.0f;
-
     Coroutine killEnemy;
 
     DetectSafeZone detector; //player in safezone trigger
@@ -19,9 +18,13 @@ public class EnemyDeath : EnemyBase
     // ====audio==== //
 
     [SerializeField] AudioSource aud;
+
     [SerializeField] AudioClip[] audChase;
     [UnityEngine.Range(0, 1)][SerializeField] float audChaseVol;
-    
+
+    [SerializeField] AudioClip[] audDestroy;
+    [UnityEngine.Range(0, 1)][SerializeField] float audDestroyVol;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,48 +38,70 @@ public class EnemyDeath : EnemyBase
     // Update is called once per frame
     protected override void Update()
     {
+        base.Update();
 
-        if (detector != null && detector.InSafeZone)
+        if (PlayerSafe())
         {
+            EscapefromPlayer();
+
+            //start destroy timer
             if (killEnemy == null)
             {
                 killEnemy = StartCoroutine(IfEnemySafe());
             }
+
         }
         else
         {
+            //don't destroy if enemy out
             if (killEnemy != null)
             {
                 StopCoroutine(killEnemy);
                 killEnemy = null;
             }
+
+            chasePlayer();
         }
 
-        //if (detector)
-        //{
-        //    if (killEnemy == null) killEnemy = StartCoroutine(IfEnemySafe());
-        //}
-        //else
-        //{
-        //    if (killEnemy != null) { StopCoroutine(killEnemy); killEnemy = null; }
-        //}
-
-
-        startChasing();
+        if (timerChase > 0)
+        {
+            timerChase -= Time.deltaTime;
+            return;
+        }
     }
 
     IEnumerator IfEnemySafe()
     {
-        //audio
 
         yield return new WaitForSeconds(selfDestruct);
 
-        //roam
-
-        if (detector != null && detector.InSafeZone)
+        if (PlayerSafe())
         {
+            if (audDestroy.Length > 0)
+                aud.PlayOneShot(audDestroy[Random.Range(0, audDestroy.Length)], audDestroyVol);
+
             Destroy(gameObject);
+        }
             killEnemy = null;
+    }
+
+    private bool PlayerSafe()
+    {
+        return (detector != null && detector.InSafeZone);
+    }
+
+    void EscapefromPlayer()
+    {
+        if (agent == null || gameManager.instance.player == null) return;
+
+        Vector3 dirAway = (transform.position - gameManager.instance.player.transform.position).normalized;
+        Vector3 escapePos = transform.position + dirAway * escapeDistance;
+
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(escapePos, out hit, escapeDistance, NavMesh.AllAreas))
+        {
+            agent.SetDestination(hit.position);
+            agent.speed = chaseSpeed * 0.8f; //slower than chase
         }
     }
 
@@ -85,20 +110,8 @@ public class EnemyDeath : EnemyBase
         agent.speed = chaseSpeed;
         base.chasePlayer();
 
-        aud.PlayOneShot(audChase[UnityEngine.Random.Range(0, audChase.Length)], audChaseVol); 
+       // aud.PlayOneShot(audChase[UnityEngine.Random.Range(0, audChase.Length)], audChaseVol);
 
-    }
-
-    private void startChasing()
-    {
-
-        if (timerChase > 0)
-        {
-            timerChase -= Time.deltaTime;
-            return;
-        }
-
-        chasePlayer();
     }
 
     public void ChaseNow()
@@ -106,10 +119,11 @@ public class EnemyDeath : EnemyBase
         timerChase = 0;
     }
 
-    //bool PlayerOnNavMesh()
-    //{
-    //    return NavMesh.SamplePosition(gameManager.instance.player.transform.position, out _, playerNavmeshRadius, NavMesh.AllAreas); 
-    //    //checks around player if theres navmesh
-    //didn't worked: too many spots without navmesh (not reliable)
-    //}
+   /* bool PlayerOnNavMesh()
+    {
+        return NavMesh.SamplePosition(gameManager.instance.player.transform.position, out _, playerNavmeshRadius, NavMesh.AllAreas);
+        //checks around player if theres navmesh
+        didn't worked: too many spots without navmesh (not reliable)
+    }
+   */
 }
